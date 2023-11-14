a5260a02998f809cda57cb86f82ade35d24533da
Riwalenn Bas <riwalenn@gmail.com>
2023-11-14 20:25:56 +0100

    lesson on dynamic component

---
+++ src/components/About.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<template>
+  <h1>About page</h1>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "Home",
+  unmounted() {
+    console.log("Home component unmounted")
+  },
+  activated() {
+    console.log("Home component activated")
+  },
+  deactivated() {
+    console.log("Home component deactivated")
+  }
+}
+</script>
+
+<template>
+  <h1>Home Page</h1>
+</template>
